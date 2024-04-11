02bd6ab2d211fa601c31813f13c7e6bd159330d4
archkras <vpa_kras@rambler.ru>
2024-04-11 12:43:58 +0700

    fix path to voices

--- __init__.py
+++ __init__.py
@@ -3,9 +3,7 @@ import os
 from .install import install_styleTTS2_fix, uninstall_styleTTS2_fix
 
 root_path = "StyleTTS2/"
-if os.path.isdir(f"{root_path}Models") and os.path.isdir(
-    f"{root_path}Demo/reference_audio"
-):
+if os.path.isdir(f"{root_path}Models") and os.path.isdir(f"{root_path}voices"):
     print("StyleTTS2 data exist")
 else:
     print("StyleTTS2 data not exist\n remove previous data")
